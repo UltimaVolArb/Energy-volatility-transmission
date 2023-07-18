a7ba4bd644070e8bc251425785420edddcb1aead
Rand() <103608797+ergodic-metrika@users.noreply.github.com>
2023-07-18 12:17:29 -0400

    Add files via upload

---
+++ MBS research.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 27 12:34:17 2022
+
+@author: sigma
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib import pyplot
+import numpy as np
+import pandas as pd
+
+import scipy.stats
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
